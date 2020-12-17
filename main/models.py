482ed811b3faa83f987b0716c4cbe0602f639c1e
Ilia <kul.2002.il@gmail.com>
2020-12-17 12:38:46 +0700

    Index done

--- main/models.py
+++ main/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Product(models.Model):
 	title = models.CharField(max_length=255, verbose_name='Название')
-	published_date = models.DateTimeField(verbose_name='Дата публикации')
+	published_date = models.DateTimeField(verbose_name='Дата публикации', default=timezone.now)
+
+	def __str__(self):
+		return self.title
