dcc1fafc1df9de83d1a0cf3d48d446c3cda4d087
Ilia <kul.2002.il@gmail.com>
2020-12-18 12:32:54 +0700

    Добавлены изображения.

--- main/admin.py
+++ main/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 
 from.models import *
 
+admin.site.register(UserProfile)
 admin.site.register(Product)
+admin.site.register(PhotoProduct)
--- main/models.py
+++ main/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 from django.utils import timezone
 
+from django.contrib.auth.models import AbstractUser
+
+
+class UserProfile(AbstractUser):
+	image = models.ImageField(verbose_name="Аватарка")
+
 
 class Product(models.Model):
 	title = models.CharField(max_length=255, verbose_name='Название')
+	description = models.TextField(verbose_name='Описание товара', default='')
 	published_date = models.DateTimeField(verbose_name='Дата публикации', default=timezone.now)
 
 	def __str__(self):
 		return self.title
+
+
+class PhotoProduct(models.Model):
+	image = models.ImageField(verbose_name="Фото товара")
+	product = models.ForeignKey(Product, on_delete=models.CASCADE)
